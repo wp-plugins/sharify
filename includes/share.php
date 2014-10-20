b9de1565d1bb38976bda813d84aeb8c3ff8b3dad
imehedidip <imehedidip@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-20 17:53:16 +0000

    Fixed bugs - V1.1
    git-svn-id: https://plugins.svn.wordpress.org/sharify/tags/1.1@1010783 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/share.php
@@ -0,0 +1,77 @@
+<?php
+
+// Get Post Title and Permalink
+$title = get_the_title();
+$url   = get_permalink();
+
+// Get Share Count
+require_once "share_count.php";
+$SharifyCount  = new SharifyCount($url);
+$facebookCount = $SharifyCount->getFacebookCount();
+
+echo '
+		<div class="si-share-box">
+			<a href="https://twitter.com/intent/tweet?text=' . $title . ' - ' . $url . '" onclick="window.open(this.href, \'mywin\',
+		    \'left=50,top=50,width=600,height=350,toolbar=0\'); return false;">
+			<div class="si-button si-button-twitter" title="Tweet on Twitter">
+				<div class="si-share-main">
+					<i class="sharify-twitter sharify"></i>
+					<p class="si-share-text">Tweet</p>
+				</div>
+				<div class="si-share-counter"><p class="si-share-count-text">' . $SharifyCount->getTweetCount() . '</p></div>
+			</div>
+			</a>
+
+			<a href="http://www.facebook.com/sharer.php?u=' . urlencode($url) . '" onclick="window.open(this.href, \'mywin\',
+			\'left=50,top=50,width=600,height=350,toolbar=0\'); return false;">
+			<div class="si-button si-button-facebook" title="Share link on Facebook">
+				<div class="si-share-main">
+					<i class="sharify-facebook sharify"></i>
+					<p class="si-share-text">Like</p>
+				</div>
+				<div class="si-share-counter"><p class="si-share-count-text">' . $facebookCount['share_count'] . '</p></div>
+			</div>
+			</a>
+			
+			<a href="http://plus.google.com/share?url=' . $url . '" onclick="window.open(this.href, \'mywin\',
+			\'left=50,top=50,width=600,height=350,toolbar=0\'); return false;">
+			<div class="si-button si-button-gplus" title="Share link on Google+">
+				<div class="si-share-main">
+					<i class="sharify-gplus sharify"></i>
+					<p class="si-share-text">Share</p>
+				</div>
+				<div class="si-share-counter"><p class="si-share-count-text">' . get_plusones_si($url) . '</p></div>
+			</div>
+			</a>
+
+			<a href="https://getpocket.com/save?url=' . urlencode($url) . '&amp;media=' . (!empty($image[0]) ? $image[0] : '') . '" onclick="window.open(this.href, \'mywin\',
+			\'left=50,top=50,width=600,height=350,toolbar=0\'); return false;">
+			<div class="si-button-small si-button-pocket" title="Save to Pocket">
+				<i class="sharify-pocket sharify-small sharify"></i>
+			</div>
+			</a>
+
+			<a class="si-share-box" href="http://reddit.com/submit?url=' . $url . '" onclick="window.open(this.href, \'mywin\',
+			\'left=50,top=50,width=600,height=350,toolbar=0\'); return false;">
+			<div class="si-button-small si-button-reddit"  title="Share link on Reddit">
+				<i class="sharify-reddit sharify sharify-small"></i>
+			</div>
+			</a>
+
+			<a class="si-share-box" href="https://linkedin.com/shareArticle?mini=true&url=' . $title . ' | ' . $url . ' &via=' . urlencode($twetter_user ? $twetter_user : get_bloginfo('name')) . '" onclick="if(!document.getElementById(\'td_social_networks_buttons\')){window.open(this.href, \'mywin\',
+			\'left=50,top=50,width=600,height=350,toolbar=0\'); return false;}" >
+			<div class="si-button-small si-button-linkedin" title="Share link on LinkedIn">
+				<i class="sharify-linkedin sharify sharify-small"></i>
+			</div>
+			</a>
+
+			<a class="si-share-box" href="http://pinterest.com/pin/create/button/?url=' . $url . '&amp;media=' . (!empty($image[0]) ? $image[0] : '') . '" onclick="window.open(this.href, \'mywin\',
+			\'left=50,top=50,width=600,height=350,toolbar=0\'); return false;">
+			<div class="si-button-small si-button-pinterest" title="Pin it on Pinterest">
+				<i class="sharify-pinterest sharify sharify-small"></i>
+			</div>
+			</a>
+
+		</div>
+	';
+?>
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Sharify ===
+Contributors: Mehedi
+Tags: share,social,sharing buttons,responsive sharing buttons,social network
+Requires at least: 3.0
+Tested up to: 3.6
+Stable tag: 1.1
+Version: 1.1
+License: GPL
+
+Sharify will give you the perfect sharing buttons for your website. It is fast, responsive and you'll love it!
+
+== Description ==
+Sharify will give you the perfect sharing buttons you've been looking for! It is responsive, well-designed and it simply looks awesome! It is really easy to setup as well :)
+
+Features
+- Responsive
+- Fast
+- Shortcode support
+
+Use the shortcode "[sharify]" to display the buttons anywhere you want!
+
+Thanks for using my plugin :) I will be updating the plugin with some new features soon.
+
+== Installation ==
+Installing Sharify is easy! Just follow this steps:
+
+1. Login to your WordPress Dashboard
+2. Plugins>Add New>Upload
+3. Select "sharify.zip" and hit Upload
+4. Activate the plugin and that's it! It will now appear on your posts!
+
+== Frequently Asked Questions ==
+1. Can I remove any social network?
+- Unfortunately, you can't remove a social network yet. However, I will add the feature on the next version.
+
+2. Is there any shortcode?
+- Yes. Just use "[sharify]"
+
+3. I want to request a sharing button. How can I do that?
+- Request it on the Support tab.
+
+== Screenshots ==
+1. The desktop view of the plugin
+2. The tablet view of the plugin
+3. The smartphone view of the plugin
+
+== Changelog ==
+Version 1.1
+- Fixed bugs
+- Removed auto additon of the sharebutton. (use [sharify] to display the buttons)
+-----------------------------
+Version 1.0
+- Initial Release
