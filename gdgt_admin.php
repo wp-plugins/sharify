099b8754be0dd820349e442e3d1486d7db35b31b
imehedidip <imehedidip@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-01 19:03:23 +0000

    !.5.3 with bug fixes and new features
    git-svn-id: https://plugins.svn.wordpress.org/sharify/tags/1.5.3@1036120 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gdgt_admin.php
@@ -0,0 +1,35 @@
+<?php
+
+//Regsiter Settings
+function admin_init_sharify() {
+	register_setting('sharify', 'display_button_facebook');
+	register_setting('sharify', 'display_button_linkedin');
+	register_setting('sharify', 'display_button_twitter');
+	register_setting('sharify', 'display_button_reddit');
+	register_setting('sharify', 'display_button_google');
+	register_setting('sharify', 'display_button_pocket');
+	register_setting('sharify', 'display_button_pinterest');
+	register_setting('sharify', 'display_buttons_under_post');
+	register_setting('sharify', 'load_google_fonts');
+}
+
+//Add Options Page
+function admin_menu_sharify() {
+  add_options_page(
+		'Sharify',
+		'Sharify Settings',
+		'manage_options',
+		'sharify',
+		'options_page_sharify');
+}
+
+function options_page_sharify() {
+  include( 'admin_options.php' );  
+}
+
+if (is_admin()) {
+  add_action('admin_init', 'admin_init_sharify');
+  add_action('admin_menu', 'admin_menu_sharify');
+}
+
+?>
---
+++ includes/share_count.php
@@ -0,0 +1,48 @@
+<?php
+
+function sharesCounter($sharify_url = '', $echo = true, $facebook = true, $twitter = true, $gplus = true, $linkedin = true) {
+    $shares = 0; 
+    if ( !isset($sharify_url) || $sharify_url === '' ) {
+        if ( isset($_SERVER['https']) ) { 
+            $protocol = 'https://';
+        } else {
+            $protocol = 'http://'; 
+        }
+        $sharify_url =  $protocol . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    }
+    if ( $facebook ) { // Facebook is active?
+        $sharify_url_fb = "http://api.facebook.com/restserver.php?method=links.getStats&urls=" . $sharify_url; 
+        $data_fb = simplexml_load_file($sharify_url_fb); 
+        if ( isset($data_fb->link_stat->share_count) ) { 
+            $shares += $data_fb->link_stat->share_count; 
+        }
+    }
+    if ( $twitter ) { // Twitter is active?
+        $sharify_url_tw = "http://urls.api.twitter.com/1/urls/count.json?url=" . $sharify_url;
+        $data_tw = json_decode(file_get_contents($sharify_url_tw)); 
+        if ( isset($data_tw->count) ) { 
+            $shares = $shares + $data_tw->count; 
+        }
+    }
+    if ( $gplus ) { 
+        $curl = curl_init(); 
+        curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc"); 
+        curl_setopt($curl, CURLOPT_POST, 1); 
+        curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $sharify_url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]'); // Defines the fields of the POST request
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        $curl_results = curl_exec ($curl); 
+        curl_close ($curl); 
+        $json = json_decode($curl_results, true); 
+        if ( isset($json) && isset($json[0]['result']['metadata']['globalCounts']['count']) ) { 
+            $result = intval($json[0]['result']['metadata']['globalCounts']['count']); 
+            $shares += $result; 
+        }
+    }
+    if ( $echo ) { 
+        echo intval($shares); 
+    } else {
+        return intval($shares); 
+    }
+}
+?>
