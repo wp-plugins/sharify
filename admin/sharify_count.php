fd9b72d9c7351be80df0ee06cf91a0a80b5d1f12
imehedidip <imehedidip@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-15 19:02:17 +0000

    Sharify 1.9.3 bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/sharify/tags/1.9.3@1068789 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/sharify_count.php
@@ -0,0 +1,32 @@
+<?php
+
+//Get Tweet Count
+function sharify_get_tweets($url) {
+    $twittercount = json_decode( file_get_contents( 'http://urls.api.twitter.com/1/urls/count.json?url='.$url ) );
+    return $twittercount->count;
+}
+
+//Get Facebook Share Count
+function sharify_get_share($url) {
+    $url = urlencode($url);
+    $facebookcount = json_decode( file_get_contents( 'http://graph.facebook.com/'.$url ) );
+    return $facebookcount->shares;   
+}
+
+//Get Plus One Count
+function sharify_get_plus($url) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    $curl_results = curl_exec ($curl);
+    curl_close ($curl);
+
+    $json = json_decode($curl_results, true);
+
+    return intval( $json[0]['result']['metadata']['globalCounts']['count'] );
+}
+?>
