4d9fccdab448d5c9f02bf62c9cf9e408da7ad4cb
imehedidip <imehedidip@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-27 15:29:26 +0000

    Sharify 3.4.4!
    git-svn-id: https://plugins.svn.wordpress.org/sharify/trunk@1188622 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sharify.php
+++ sharify.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wordpress.org/plugins/sharify/
  * Description: Sharify is a fast and simple plugin for sharing buttons on WordPress. The plugin lets you display responsive sharing 
  * buttons on your WordPress website!
- * Version: 3.4.3
+ * Version: 3.4.4
  * Author: imehedidip
  * Author URI: http://twitter.com/mehedih_
  * Text Domain: sharify
